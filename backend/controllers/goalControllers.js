4aa5144d85b829d525670d2d7530c2f9f4401a9f
Akinola Kehinde <khennyakin@gmail.com>
2024-09-30 23:58:27 +0100

    created a function to delete a goal using id

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -11,6 +11,13 @@ export const getGoal = (req, res, next) => {
   res.status(200).json({ msg: `Post with the id: ${id} can not be found` });
 };
 
+// @desc Delete goal
+// @ DELETE /api/goals/:id
+export const deleteGoal = (req, res, next) => {
+  const id = req.params.id;
+  res.status(200).json({ msg: `Post with the id: ${id} can not be found` });
+};
+
 // @desc Post new goal
 // @ POST /api/goals/
 export const postGoal = (req, res, next) => {
--- backend/route/goalRoutes.js
+++ backend/route/goalRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getGoals, postGoal, getGoal } from "../controllers/goalControllers.js";
+import {
+  getGoals,
+  postGoal,
+  getGoal,
+  deleteGoal,
+} from "../controllers/goalControllers.js";
 
 const router = express.Router();
 
 router.route("/").get(getGoals).post(postGoal);
-router.route("/:id").get(getGoal);
+router.route("/:id").get(getGoal).delete(deleteGoal);
 
 export default router;
